c3fbe55f4fe246208b9c44bc0b290fb11fcd0947
Artur Grabowski <art@blahonga.org>
2017-01-20 16:57:41 +0100

    With some cheating and hacking, we now have shiny windows (resizable).

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	"kk/kk"
 	"log"
 
-	"golang.org/x/mobile/app"
+	"golang.org/x/exp/shiny/driver/gldriver"
+	"golang.org/x/exp/shiny/screen"
 	"golang.org/x/mobile/event/key"
 	"golang.org/x/mobile/event/lifecycle"
 	"golang.org/x/mobile/event/paint"
@@ -12,64 +13,111 @@ import (
 	"golang.org/x/mobile/gl"
 )
 
-func main() {
-	mmain()
+type state struct {
+	tiles *kk.Tiles
+	f     *kk.Field
+	wsz   size.Event
+	glctx gl.Context
 }
 
-func mmain() {
-	app.Main(func(a app.App) {
-		var glctx gl.Context
+func st() *state {
+	s := state{f: kk.NewField()}
+	return &s
+}
 
-		repaint := false
-		wsz := size.Event{}
-		f := kk.NewField()
+func (s *state) handle(ei interface{}) (repaint bool, quit bool, publish bool) {
+	switch e := ei.(type) {
+	case lifecycle.Event:
+		if e.To == lifecycle.StageDead {
+			quit = true
+			return
+		}
+		switch e.Crosses(lifecycle.StageVisible) {
+		case lifecycle.CrossOn:
+			s.glctx, _ = e.DrawContext.(gl.Context)
+			s.tiles = kk.NewTiles(s.glctx)
+			repaint = true
+		case lifecycle.CrossOff:
+			s.glctx = nil
+			quit = true
+			return
+		}
 
-		var tiles *kk.Tiles
+	case key.Event:
+		if e.Code == key.CodeEscape {
+			quit = true
+			return
+		}
+		if e.Direction == key.DirPress {
+			switch e.Code {
+			case key.CodeLeftArrow:
+				s.f.Left()
+			case key.CodeRightArrow:
+				s.f.Right()
+			case key.CodeUpArrow:
+				s.f.Up()
+			case key.CodeDownArrow:
+				s.f.Down()
+			}
+			repaint = true
+		}
 
-		for e := range a.Events() {
-			switch e := a.Filter(e).(type) {
-			case lifecycle.Event:
-				switch e.Crosses(lifecycle.StageVisible) {
-				case lifecycle.CrossOn:
-					glctx, _ = e.DrawContext.(gl.Context)
-					tiles = kk.NewTiles(glctx)
-					a.Send(paint.Event{})
-				case lifecycle.CrossOff:
-					glctx = nil
-					return
-				}
+	case paint.Event:
+		kk.Draw(s.glctx, s.wsz, s.f, s.tiles)
+		publish = true
+	case size.Event:
+		s.wsz = e
 
-			case key.Event:
-				if e.Code == key.CodeEscape {
-					return
-				}
-				if e.Direction == key.DirPress {
-					switch e.Code {
-					case key.CodeLeftArrow:
-						f.Left()
-					case key.CodeRightArrow:
-						f.Right()
-					case key.CodeUpArrow:
-						f.Up()
-					case key.CodeDownArrow:
-						f.Down()
-					}
-					if !repaint {
-						repaint = true
-						a.Send(paint.Event{})
-					}
-				}
+	case error:
+		log.Print(e)
+	}
+	return
+}
 
-			case paint.Event:
-				kk.Draw(glctx, wsz, f, tiles)
-				a.Publish()
-				repaint = false
-			case size.Event:
-				wsz = e
+func main() {
+	smain()
+}
 
-			case error:
-				log.Print(e)
+func smain() {
+	gldriver.Main(func(s screen.Screen) {
+		st := st()
+		w, err := s.NewWindow(&screen.NewWindowOptions{400, 400})
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer w.Release()
+		for {
+			repaint, quit, publish := st.handle(w.NextEvent())
+			if quit {
+				return
+			}
+			if repaint {
+				w.Send(paint.Event{})
+			}
+			if publish {
+				w.Publish()
 			}
 		}
 	})
 }
+
+/*
+func mmain() {
+	app.Main(func(a app.App) {
+		s := st()
+
+		for e := range a.Events() {
+			repaint, quit, publish := s.handle(a.Filter(e))
+			if quit {
+				return
+			}
+			if repaint {
+				a.Send(paint.Event{})
+			}
+			if publish {
+				a.Publish()
+			}
+		}
+	})
+}
+*/
