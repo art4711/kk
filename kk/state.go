0d1e5b31d65552f30182225f22f4000f6ab01d2c
Artur Grabowski <art@blahonga.org>
2017-01-25 12:49:26 +0100

    Merge some stuff.

--- kk/state.go
+++ kk/state.go
@@ -1,12 +1,14 @@
 package kk
 
 import (
+	"image"
 	"log"
 
 	"golang.org/x/mobile/event/key"
 	"golang.org/x/mobile/event/lifecycle"
 	"golang.org/x/mobile/event/paint"
 	"golang.org/x/mobile/event/size"
+	"golang.org/x/mobile/geom"
 	"golang.org/x/mobile/gl"
 )
 
@@ -59,7 +61,7 @@ func (s *State) Handle(ei interface{}) (repaint bool, quit bool, publish bool) {
 		}
 
 	case paint.Event:
-		Draw(s.glctx, s.wsz, s.f, s.tiles)
+		s.Draw()
 		publish = true
 	case size.Event:
 		s.wsz = e
@@ -68,4 +70,22 @@ func (s *State) Handle(ei interface{}) (repaint bool, quit bool, publish bool) {
 		log.Print(e)
 	}
 	return
+}
+
+func (s *State) Draw() {
+	s.glctx.ClearColor(0, 0, 0, 1)
+	s.glctx.Clear(gl.COLOR_BUFFER_BIT)
+	// We actually want to create the tiles with correct pixel sizes so the font looks good ...
+	tsz := image.Point{s.wsz.WidthPx / s.f.W(), s.wsz.HeightPx / s.f.H()}
+	// ..., but we need to render them in geom space.
+	tpsz := geom.Point{s.wsz.WidthPt / geom.Pt(s.f.W()), s.wsz.HeightPt / geom.Pt(s.f.H())}
+	for y := 0; y < s.f.H(); y++ {
+		for x := 0; x < s.f.W(); x++ {
+			t := s.tiles.Get(s.f[y][x], tsz)
+			tl := geom.Point{tpsz.X * geom.Pt(x), tpsz.Y * geom.Pt(y)}
+			tr := geom.Point{tpsz.X * geom.Pt(x+1), tpsz.Y * geom.Pt(y)}
+			bl := geom.Point{tpsz.X * geom.Pt(x), tpsz.Y * geom.Pt(y+1)}
+			t.Draw(s.wsz, tl, tr, bl, image.Rectangle{Max: tsz})
+		}
+	}
 }
